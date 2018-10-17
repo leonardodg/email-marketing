ee79a5772ab7a2697c480abe33f9c901700f07f5
João henrique de moura gonçalves <jhg1802@gmail.com>
2018-10-17 17:57:54 -0300

    ajustes css, gulp inline css, cellpadding + cellspacing + border = noborder class css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp'),
     inlineCss = require('gulp-inline-css');
- 
+
 gulp.task('default', function() {
-    return gulp.src('./*.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest('/inline'));
+    return gulp.src('index.html')
+        .pipe(inlineCss({
+        	preserveMediaQueries:true,
+        	removeStyleTags:false
+        }))
+        .pipe(gulp.dest('inline/'));
 });
