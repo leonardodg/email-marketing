e3f923fd12d582fb370fed144f5f4bc56bcef45d
João henrique de moura gonçalves <jhg1802@gmail.com>
2018-11-06 18:15:36 -0300

    arquivo scss em ordem e comentado

---
+++ config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "estilos"
+sass_dir = "sass"
+
+
+output_style = :expanded
+#:nested, :expanded, :compact, ou :compressed
+
+#relative_assets = true 
+
+
+line_comments = false
---
+++ css backup/config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+
+
+output_style = :expanded
+
+
+relative_assets = true 
+
+
+line_comments = true
