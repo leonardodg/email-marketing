4cc753e263042fae007a1e75776b8ca1108a1232
João henrique de moura gonçalves <jhg1802@gmail.com>
2018-11-14 17:58:54 -0300

    Variaveis sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp'),
-    inlineCss = require('gulp-inline-css');
+    inlineCss = require('gulp-inline-css'),
+	styleInject = require("gulp-style-inject");    
 
 gulp.task('default', function() {
     return gulp.src('index.html')
+		.pipe(styleInject())    
         .pipe(inlineCss({
-        	preserveMediaQueries:true,
-        	removeStyleTags:false
+        	preserveMediaQueries:false,
+        	removeStyleTags:false,
+        	applyStyleTags:false
+        	//applyLinkTags : false
         }))
         .pipe(gulp.dest('inline/'));
 });
